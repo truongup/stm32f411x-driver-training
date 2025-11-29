bce20b58efcd07ae64577f2e6a0bde366edf55f0
giangtruong1007 <giangtruong100702@gmail.com>
2025-11-29 12:48:12 +0700

    Define GPIO Register and RCC Register

--- drivers/Inc/stm32f411x.h
+++ drivers/Inc/stm32f411x.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+#define __vo volatile
+
 /*	MEMORY ADDRESS DEFINE */
 #define FLASH_BASE_ADDR		0x08000000U		/* Flash address */
 #define SRAM1_BASE_ADDR		0x20000000U		/* SRAM address  */
@@ -54,5 +56,86 @@
 
 /*	SYSTEM CONFIG	*/
 #define SYSCFG_BASE_ADDR	(APB2_PERIPHERAL_BASE_ADDR + 0x3800)	/*	Quan trọng vì nó dùng để tạo ngắt */
+
+
+/*	------------ GPIO AHB1 ---------------- */
+typedef struct {
+	__vo uint32_t MODER;			// port mode register
+	__vo uint32_t OTYPER;			// output type register
+	__vo uint32_t OSPEEDR;			// output speed register
+	__vo uint32_t PUPDR;			// port pull up / down register
+	__vo uint32_t IDR;				// input data register
+	__vo uint32_t ODR;				// output data register
+	__vo uint32_t BSRR;				// bit set / reset register
+	__vo uint32_t LCKR;				// port configuration lock register
+	__vo uint32_t AFR[2];			// alternate function low / high register
+}GPIO_Def_t;
+
+
+#define GPIOA   ((GPIO_Def_t *)GPIOA_BASE_ADDR)
+#define GPIOB   ((GPIO_Def_t *)GPIOB_BASE_ADDR)
+#define GPIOC   ((GPIO_Def_t *)GPIOC_BASE_ADDR)
+#define GPIOD   ((GPIO_Def_t *)GPIOD_BASE_ADDR)
+#define GPIOE   ((GPIO_Def_t *)GPIOE_BASE_ADDR)
+#define GPIOH   ((GPIO_Def_t *)GPIOH_BASE_ADDR)
+
+/*	------------ RCC AHB1 ---------------- */
+typedef struct {
+    __vo  uint32_t CR;          // clock control register
+    __vo  uint32_t PLLCFGR;     // configuration register
+    __vo  uint32_t CFGR;        // clock configuration register
+    __vo  uint32_t CIR;         // clock interrupt register
+
+    __vo  uint32_t AHB1RSTR;    // AHB1 peripheral reset register
+    __vo  uint32_t AHB2RSTR;    // AHB2 peripheral reset register
+
+    uint32_t RESERVED_0;
+    uint32_t RESERVED_1;
+
+    __vo  uint32_t APB1RSTR;    // APB1 peripheral reset register for
+    __vo  uint32_t APB2RSTR;    // APB2 peripheral reset register for
+
+    uint32_t RESERVED_2;
+    uint32_t RESERVED_3;
+
+    __vo  uint32_t AHB1ENR;     // AHB1 peripheral clock enable register
+    __vo  uint32_t AHB2ENR;     // AHB2 peripheral clock enable register
+
+    uint32_t RESERVED_4;
+    uint32_t RESERVED_5;
+
+    __vo  uint32_t APB1ENR;     // APB1 peripheral clock enable register
+    __vo  uint32_t APB2ENR;     // APB2 peripheral clock enable register
+
+    uint32_t RESERVED_6;
+    uint32_t RESERVED_7;
+
+    __vo  uint32_t AHB1LPENR;   // AHB1 clock enable in low power mode register
+    __vo  uint32_t AHB2LPENR;   // AHB2 clock enable in low power mode register
+
+    uint32_t RESERVED_8;
+    uint32_t RESERVED_9;
+
+    __vo  uint32_t APB1LPENR;   // APB1 clock enable in low power mode regiser
+    __vo  uint32_t APB2LPENR;   // APB2 clock enable in low power mode regiser
+
+    uint32_t RESERVED_10;
+    uint32_t RESERVED_11;
+
+    __vo  uint32_t BDCR;        // backup domain control register
+    __vo  uint32_t CSR;         // clock control & status register
+
+    uint32_t RESERVED_12;
+    uint32_t RESERVED_13;
+
+    __vo  uint32_t SSCGR;       // spread spectrum clock generation register
+    __vo  uint32_t PLLI2SCFGR;  // PLLI2S configuration register
+
+    uint32_t RESERVED_14;
+    __vo  uint32_t DCKCFGR;     // Dedicated clocks configuration register
+}RCC_Def_t;
+
+#define RCC ((RCC_Def_t *) RCC_BASE_ADDR)
+
 
 #endif
